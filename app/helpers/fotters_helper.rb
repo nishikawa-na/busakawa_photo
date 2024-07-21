984e0c042933a6efee4e3b8742e110d467d75c5c
nishikawa <nishikawa_ak_na@outlook.jp>
2024-07-21 14:40:08 +0900

    Add フッター作成

---
+++ app/helpers/fotters_helper.rb
@@ -0,0 +1,2 @@
+module FottersHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "static_pages#index"
+  get "inquiry", to: "fotters#inquiry"
+  get "terms", to: "fotters#terms"
+  get "policy", to: "fotters#policy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
