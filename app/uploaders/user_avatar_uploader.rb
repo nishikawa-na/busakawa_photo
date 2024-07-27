53a8d4d36e53ad40f14d27449eb43478c853e462
nishikawa <nishikawa_ak_na@outlook.jp>
2024-07-28 06:30:58 +0900

    Add AWS S3設定追加

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -4,7 +4,13 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
   def default_url
     'profile.png'
