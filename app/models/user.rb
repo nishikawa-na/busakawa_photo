544f792724e8bcfe78527e69a06a2926b46b41e9
nishikawa <nishikawa_ak_na@outlook.jp>
2024-08-19 19:54:54 +0900

    Add パスワード変更機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] } 
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :reset_password_token, uniqueness: true, allow_nil: true
   mount_uploader :avatar, UserAvatarUploader
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :like_posts, dependent: :destroy
+
 end
