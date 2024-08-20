27135754c192f3a34fdd7292e510045f7767a5e3
nishikawa <nishikawa_ak_na@outlook.jp>
2024-08-20 20:19:42 +0900

    Add パスワード変更リンクをログインページへ追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,10 +1,11 @@
 class PasswordResetsController < ApplicationController
+  skip_before_action :require_login
   def new; end
   
   def create
     @user = User.find_by_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
-    redirect_to posts_path
+    redirect_to login_path
   end
 
   def edit
@@ -28,7 +29,7 @@ class PasswordResetsController < ApplicationController
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to posts_path
+      redirect_to login_path
     else
       render :action => "edit"
     end
