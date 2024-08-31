1ab26fa6d486522cfbbade8b372bc5d19bdc5d26
nishikawa <nishikawa_ak_na@outlook.jp>
2024-08-31 11:42:11 +0900

    Add フォロー機能作成

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+class RelationshipsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(params[:user_id])
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(params[:user_id])
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_045940) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_30_053537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_29_045940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
