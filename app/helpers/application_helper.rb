42cfb13178b8cfc81c4b25846e85ddc0f73056b2
nishikawa <nishikawa_ak_na@outlook.jp>
2024-09-29 13:03:44 +0900

    Fix meta_tagsの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,6 @@ module ApplicationHelper
     site = options[:site]
     title = options[:title]
     description = options[:description]
-    keywords = options[:keywords]
     image = options[:image].presence || image_url('profile.png')
 
     configs = {
@@ -29,7 +28,6 @@ module ApplicationHelper
       site:,
       title:,
       description:,
-      keywords:,
       canonical: request.original_url,
       icon: {
         href: image_url('profile.png')
@@ -40,7 +38,6 @@ module ApplicationHelper
         description:,
         url: request.original_url,
         image:,
-        keywords:,
         site_name: site
       },
       twitter: {
@@ -48,7 +45,6 @@ module ApplicationHelper
         site:,
         title:,
         description:,
-        keywords:,
         image:
       }
     }
