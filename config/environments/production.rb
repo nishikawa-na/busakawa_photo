f95c6e1cb6465870ff9ae9c8f3e2794235ab7c47
nishikawa <nishikawa_ak_na@outlook.jp>
2024-10-05 09:04:58 +0900

    Add 本番環境メール送信不具合にて修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,9 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.default_url_options = { host:'https://busakawa.com'}
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'busakawaphoto@gmail.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
