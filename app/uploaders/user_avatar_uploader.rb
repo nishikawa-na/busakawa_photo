fb156ddc68f700da539f0a81a08ff6126e2788e7
nishikawa <nishikawa_ak_na@outlook.jp>
2025-05-24 19:30:11 +0900

    Add minimagickの各画像リサイズ設定追加

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -1,7 +1,7 @@
 class UserAvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-
+  include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
@@ -33,7 +33,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fit: [250, 250]
   #
   # def scale(width, height)
   #   # do something
